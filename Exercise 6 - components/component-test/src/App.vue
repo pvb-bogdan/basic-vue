a79240417d4455ba3b6f419b08fbf653c68c0a37
Bogdan Petre <petrebogdan@Bogdans-iMac.local>
2019-02-06 14:54:14 +0200

    components and exervise

---
+++ Exercise 6 - components/component-test/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <hr>
+    <div class="row">
+      <servers></servers>
+      <app-server-details></app-server-details>
+    </div>
+    <hr>
+    <app-server-info></app-server-info>
+  </div>
+</template>
+
+<script>
+import Header from "./Shared/Header.vue";
+import ServerDetails from "./Servers/ServersDetails.vue";
+import ServerInfo from "./Servers/ServerInfo.vue";
+import Servers from "./Servers/Servers.vue";
+
+export default {
+  components: {
+    //ew could use ddifferent watsto name our comp
+    // cammelCasse
+    appHeader: Header,
+    // normal case
+    "app-server-details": ServerDetails,
+    "app-server-info": ServerInfo,
+    // ES6 case - 
+    Servers
+  }
+};
+</script>
+
+<style>
+</style>
