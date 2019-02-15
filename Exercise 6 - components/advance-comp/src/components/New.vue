c52c029e0216965afee76c885694d6737da33f2f
Bogdan Petre <petrebogdan@Bogdans-iMac.local>
2019-02-15 15:32:09 +0200

    more work on vue

---
+++ Exercise 6 - components/advance-comp/src/components/New.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p><i>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor, exercitationem?</i></p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
