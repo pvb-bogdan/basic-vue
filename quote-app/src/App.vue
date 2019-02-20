c79b2cb3022d7a51e3151f5db54dcc5e9ac3bf60
Bogdan Petre <petrebogdan@Bogdans-iMac.local>
2019-02-20 11:20:30 +0200

    finshed modules and slots app

---
+++ quote-app/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container py-3">
+    <app-header :countQuote="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @addCitat="newQuote($event)"></app-new-quote>
+    <hr>
+    <app-quote-grid :quotes="quotes" @citatSters="deleteCitat"></app-quote-grid>
+  </div>
+</template>
+
+<script>
+
+import QuoteGrid from './components/QuoteGrid';
+import NewComp from './components/NewComp';
+import Header from './components/Header'
+
+export default {
+  data: function () {
+    return {
+      quotes: [
+        'First quote'
+      ],
+      maxQuotes: 10
+    }
+  },
+  methods: {
+    newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert('ajunge');
+      } 
+      this.quotes.push(quote);
+    },
+    deleteCitat (index){
+      // console.log(index)
+      this.quotes.splice(index, 1);
+    }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewComp,
+    appHeader: Header
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ quote-app/src/components/NewComp.vue
@@ -0,0 +1,34 @@
+<template>
+    <form>
+        <div class="row">
+            <div class="col-sm-8 offset-sm-2 col-xs-12 col-md-6 offset-md-3 from-group mb-3">
+                <label>Citate</label>
+                <textarea class="form-control" v-model="quote"></textarea>
+            </div>
+            
+            <div class="col-sm-8 offset-sm-2 col-xs-12 col-md-6 offset-md-3 from-group text-center">
+                <button class="btn btn-primary" @click.prevent="createNew">Add Quote</button>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return{
+            quote: ''
+        };
+    },
+    methods: {
+        createNew () {
+            this.$emit('addCitat', this.quote);
+            this.quote = '';
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
